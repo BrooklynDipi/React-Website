939116d3cf9b6609df54968343cbb66a81ba613e
Brooklyn D <44684333+BrooklynDipi@users.noreply.github.com>
2021-03-08 13:44:57 -0600

    Edited Home Screen with Question
    Adding information questioning information about panda bears.

--- react-quiz-game-5-starter-files/src/home/home-page.js
+++ react-quiz-game-5-starter-files/src/home/home-page.js
@@ -4,6 +4,7 @@ function HomePage() {
       <h1>Home Page 🏠</h1>
       <p>This is the 🐼 panda 🐼 page.</p>
       <p>This website is dedicated to my extreme love of panda bears. 🐼🐼🐼</p>
+      <p>Why does China own all of the panda bears? Thats kinda weird.</p>
     </main>
   );
 }
