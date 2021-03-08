e9ab680d004ae62577b7f973d1aca4c8c20f3f28
Brooklyn D <44684333+BrooklynDipi@users.noreply.github.com>
2021-03-08 13:46:12 -0600

    Edited Home Page with Personal Info
    Adding information wishing I owned a panda bear.

--- react-quiz-game-5-starter-files/src/home/home-page.js
+++ react-quiz-game-5-starter-files/src/home/home-page.js
@@ -4,6 +4,7 @@ function HomePage() {
       <h1>Home Page 🏠</h1>
       <p>This is the 🐼 panda 🐼 page.</p>
       <p>This website is dedicated to my extreme love of panda bears. 🐼🐼🐼</p>
+      <p>I wish I owned a panda bear!</p>
     </main>
   );
 }
